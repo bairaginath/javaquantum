2d3a31291166a120f1e4de96ac0e36e1c687fd6a
bairaginath <mail2bairagi@gmail.com>
2015-09-04 01:36:59 +0530

    AtomicIntegerAndLong

--- javacore/com/quantum/javacore/concurrent/AtomicIntegerAndLongExample.java
+++ javacore/com/quantum/javacore/concurrent/AtomicIntegerAndLongExample.java
@@ -1,8 +1,9 @@
 package com.quantum.javacore.concurrent;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class AtomicIntegerExample {
+public class AtomicIntegerAndLongExample {
 	
 	public class ProcessingThreadWithAtomicInteger implements Runnable {
 	    private AtomicInteger count = new AtomicInteger();
@@ -18,6 +19,37 @@ public class AtomicIntegerExample {
 	 
 	 
 	    public int getCount() {
+	        return this.count.get();
+	    }
+	 
+	 
+	    private void processSomething(int i) {
+	        // processing some job
+	        try {
+	            Thread.sleep(i * 1000);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    
+	 
+	}
+	
+	public class ProcessingThreadWithAtomicLong implements Runnable {
+	    private AtomicLong count = new AtomicLong(120);
+	 
+	 
+	
+	    public void run() {
+	        for (int i = 1; i < 5; i++) {
+	            processSomething(i);
+	            count.incrementAndGet();
+	        }
+	    }
+	 
+	 
+	    public long getCount() {
 	        return this.count.get();
 	    }
 	 
@@ -67,7 +99,7 @@ public class AtomicIntegerExample {
 	}
 	public static void main(String[] args) throws InterruptedException {
    	 
-		ProcessingThreadWithAtomicInteger pt = new AtomicIntegerExample().new ProcessingThreadWithAtomicInteger();
+		ProcessingThreadWithAtomicInteger pt = new AtomicIntegerAndLongExample().new ProcessingThreadWithAtomicInteger();
         Thread t1 = new Thread(pt, "t1");
         t1.start();
         Thread t2 = new Thread(pt, "t2");
@@ -84,6 +116,16 @@ public class AtomicIntegerExample {
 //       t1.join();
 //        t2.join();
 //        System.out.println("Processing count=" + pt.getCount());
+        
+        
+    	ProcessingThreadWithAtomicLong ptl = new AtomicIntegerAndLongExample().new ProcessingThreadWithAtomicLong();
+        Thread t1long = new Thread(ptl, "t1long");
+        t1long.start();
+        Thread t2long = new Thread(ptl, "t2long");
+        t2long.start();
+        t1long.join();
+       t2long.join();
+        System.out.println("Processing count=" + ptl.getCount());
     }
     
 
